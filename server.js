c99c080d0b4fddfc7fbe01bc5e19599315333236
cascutter <cascutter13@gmail.com>
2020-10-07 10:22:30 -0500

    Everything on server.js; Functioning, but delete is not working (404 error in f12 console)

--- server.js
+++ server.js
@@ -9,25 +9,16 @@ const PORT = process.env.PORT || 8080;
 const main = path.join(__dirname, "/public");
 
 // Sets up data parsing and route middleware
-app.use(express.static(path.join(__dirname, "/db")));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-// Adds listener
-app.listen(PORT, function() {
-    console.log(`App listening on PORT ${PORT}`);
-});
-
-// Accesses route files
-//require("./routes")(app);
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(main, "/notes.html"));
 });
 
-
-// Route for api
+// Route for
 app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./db/db.json"));
 });
@@ -43,20 +34,20 @@ app.get("*", function(req, res) {
 });
 
 // Assigns ID to note
-app.post("/api/notes", function(req, res) {
+app.post("/api/notes", (req, res) => {
     let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let newNote = req.body;
     let noteId = (savedNotes.length).toString();
     newNote.id = noteId;
     savedNotes.push(newNote);
-
+    
     fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
     console.log(`Note saved! Id ${noteId}. Content:`, newNote);
     res.json(savedNotes);
 })
 
 // Deletes note based on id
-app.delete("api/notes/:id", function(req, res) {
+app.delete("api/notes/:id", (req, res) => {
     let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let noteId = req.params.id;
     let newId = 0;
@@ -64,15 +55,20 @@ app.delete("api/notes/:id", function(req, res) {
     savedNotes = savedNotes.filter(currNote => {
         return currNote.id != noteId;
     })
-
+    
     for (currNote of savedNotes) {
         currNote.id = newId.toString();
         newId++;
     }
+    
     fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
     res.json(savedNotes);
 });
 
+// Adds listener
+app.listen(PORT, function() {
+    console.log(`App listening on PORT ${PORT}`);
+});
 
 
 
