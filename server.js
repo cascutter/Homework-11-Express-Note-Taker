b20f6a2725ddfa497bc14db7f662fe0b4d114c18
cascutter <cascutter13@gmail.com>
2020-10-07 11:47:32 -0500

    Tried changing paths for 404 error

--- server.js
+++ server.js
@@ -6,69 +6,68 @@ const fs = require("fs");
 // Sets port 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const main = path.join(__dirname, "/public");
 
 // Sets up data parsing and route middleware
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(main, "/notes.html"));
+// Adds listener
+app.listen(PORT, function() {
+    console.log(`App listening on PORT ${PORT}`);
 });
 
-// Route for
+// Route for notes
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
 app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./db/db.json"));
+    res.sendFile(path.join(__dirname, "db/db.json"));
 });
 
 app.get("/api/notes/:id", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let savedNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
     res.json(savedNotes[Number(req.params.id)]);
 })
 
 // Route for index.js
 app.get("*", function(req, res) {
-    res.sendFile(path.join(main, "/index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 // Assigns ID to note
 app.post("/api/notes", (req, res) => {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let savedNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
     let newNote = req.body;
     let noteId = (savedNotes.length).toString();
     newNote.id = noteId;
     savedNotes.push(newNote);
     
-    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    fs.writeFileSync("db/db.json", JSON.stringify(savedNotes));
     console.log(`Note saved! Id ${noteId}. Content:`, newNote);
     res.json(savedNotes);
 })
 
 // Deletes note based on id
 app.delete("api/notes/:id", (req, res) => {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let savedNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
     let noteId = req.params.id;
     let newId = 0;
     console.log(`Note ${noteId} deleted.`);
-    savedNotes = savedNotes.filter(currNote => {
-        return currNote.id != noteId;
+    savedNotes = savedNotes.filter(currentNote => {
+        return currentNote.id != noteId;
     })
     
-    for (currNote of savedNotes) {
-        currNote.id = newId.toString();
+    for (currentNote of savedNotes) {
+        currentNote.id = newId.toString();
         newId++;
     }
     
-    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    fs.writeFileSync("db/db.json", JSON.stringify(savedNotes));
     res.json(savedNotes);
 });
 
-// Adds listener
-app.listen(PORT, function() {
-    console.log(`App listening on PORT ${PORT}`);
-});
 
 
 
