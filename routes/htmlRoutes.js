4b5bdffec42ef60137938f2926e1c10f6bc06652
cascutter <cascutter13@gmail.com>
2020-10-06 20:34:57 -0500

    Changed * and added console log to check for connection

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,11 +4,12 @@ module.exports = app => {
 
     // Route to notes
     app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
+        console.log("ok");
+        res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
     });
 
     // Route for index.js
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
     });
 }
