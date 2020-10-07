a17be4f860b15d8102aafed5e1297b662ee70f6a
cascutter <cascutter13@gmail.com>
2020-10-07 09:05:14 -0500

    Updated routes

--- server.js
+++ server.js
@@ -5,23 +5,20 @@ const path = require("path");
 // Sets port 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
+const main = path.join(__dirname, "/public");
 
 // Sets up data parsing and route middleware
+app.use(express.static(path.join(__dirname, "Develop/db")));
+app.use(express.static(path.join(__dirname, "Develop/public")));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "Develop/public")));
-app.use(express.static(path.join(__dirname, "../db/db")));
-
-//app.use(express.static("public"));
 
 // Accesses route files
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routes")(app);
 
 // Adds listener
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+    console.log(`App listening on PORT ${PORT}`);
 });
 
 
